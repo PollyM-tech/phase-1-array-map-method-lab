2e4602961881fe92821f3132f03d3ae8a334a639
Pauline Moraa <pmoraa431@gmail.com>
2025-03-12 04:59:28 +0300

    Using the Array Map Method

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(title => {
+    return title.replace(/\b\w/g, char => char.toUpperCase());
+  });
+};
